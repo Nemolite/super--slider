02f8451efb81c253f2108ba7b8f98c22804312eb
Nemolite <g25092011@mail.ru>
2020-04-05 06:33:53 +0300

    bxslider

---
+++ js/script.js
@@ -0,0 +1,35 @@
+console.log("test")
+$(document).ready(function(){
+    $('.bxslider-max').bxSlider(
+{
+  pager: false,
+   captions: true,
+   speed: 1000,
+   controls: false,
+   nextText: '',
+   prevText: '',
+   easing: 'jswing',
+   auto: true,
+   pause: 10000
+   }
+    	);
+    
+  });
+
+$(document).ready(function(){
+    
+    $('.bxslider-min').bxSlider(
+{
+  pager: false,
+   captions: true,
+   speed: 1000,
+   controls: false,
+   nextText: '',
+   prevText: '',
+   easing: 'jswing',
+   auto: true,
+   pause: 10000
+   }
+
+    	);
+  });
